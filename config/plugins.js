2e9fa14aa2be362d1d95353b3ca5aa6423889ba0
Ashish Kumar <36291747+akmr364@users.noreply.github.com>
2025-06-15 14:11:31 -0500

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  'wordpress-sync': {
+    enabled: true,
+    config: {
+      wordpressUrl: env('WORDPRESS_URL'),
+      syncInterval: env.int('SYNC_INTERVAL', 300000), // 5 minutes
+      webhookSecret: env('WEBHOOK_SECRET'),
+    }
+  }
+});
